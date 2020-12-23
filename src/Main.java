27132e94398c26662a006806bc682fb8cb455bd6
Elena Damdinova <elenadamdinova@gmail.com>
2020-12-23 23:27:28 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int refillBalance = 2000;
+        int bonus = 0;
+        if (refillBalance >= 1000) {
+            bonus = refillBalance / 100;
+        }
+        int totalBalance = balance + refillBalance + bonus;
+        System.out.println(totalBalance);
+    }
+}

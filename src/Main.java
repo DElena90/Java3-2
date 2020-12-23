9953246ac3a9e38616a378ae3d3a753dee7648b1
Elena Damdinova <elenadamdinova@gmail.com>
2020-12-23 23:35:00 +0800

    Исправления условия

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int refillBalance = 2000;
+        int refillBalance = 1000;
         int bonus = 0;
-        if (refillBalance >= 1000) {
+        if (refillBalance > 1000) {
             bonus = refillBalance / 100;
         }
         int totalBalance = balance + refillBalance + bonus;
